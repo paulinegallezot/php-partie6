fc3d6bdb54d385bd8fe1120cab32c324cc5f7edf
paulinegallezot <paulinegallezot@gmail.com>
2021-06-13 20:32:13 +0200

    commit

--- exo6/index.php
+++ exo6/index.php
@@ -14,7 +14,7 @@ if (isset($_GET['building'])&&isset($_GET['room'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercice 6</title>
+    <title>Exercice </title>
 </head>
 <body>
 <a href="index.php?building=12&room=101">clic</a>
